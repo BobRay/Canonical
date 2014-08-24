26ca1dd7d3e393aaa5a297940829be97ae3ade19
bobray <bobray@softville.com>
2014-08-24 01:49:08 -0500

    only execute for Symlinks

--- snippet.canonical.php
+++ snippet.canonical.php
@@ -1,28 +1,57 @@
 <?php
 /**
- * @package canonical
- * @author Bob Ray <bobray@softville.com>
+ * Canonical snippet for Canonical extra
+ *
+ * Copyright 2010-2014 by Bob Ray <http://bobsguides.com>
  * @created 07-31-2010
- * @version 1,0
- * @release beta1
-*/
-
+ *
+ * Canonical is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Canonical is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Canonical; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package canonical
+ */
 
 /******************************
 *             Usage           *
 *******************************
 
 /*
-
-Simply install the snippet with Package Manager and put the
+Install the snippet with Package Manager and put the
 following snippet tag in the <head> section of all templates
 [[!Canonical]]
 */
+/** @var $modx modX */
 
-/* Canonical snippet */
-/* Author Bob Ray */
-/* produces an appropriate canonical tag for home and other pages */
-/* put the snippet tag [[Canonical]]  in the <head> section of your template(s) */
-return $modx->resource->id == $modx->getOption('site_start') ? '<link rel="canonical" href="[[++site_url]]" />' : '<link rel="canonical" href="[[++site_url]][[~[[*id]]]]" />';
-?>
+$docId = $modx->resource->get('id');
+$query = $modx->newQuery("modSymLink");
 
+$query->select(array(
+    'id',
+    'class_key',
+    'content',
+));
+$query->where(array(
+    'id' => $docId,
+    'class_key' => 'modSymLink'
+));
+
+if ($query->prepare() && $query->stmt->execute()) {
+    $results = $query->stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+if (!empty($results)) {
+    $id = (integer) $results[0]['content'];
+    $url = $modx->makeUrl($id, '', '', 'full');
+    return '<link rel="canonical" href="' . $url . '" />';
+}
+return '';
